fe8cc377f90b79280eed081044da59d8d1ed3c63
MatthewEvans2032 <157648893+MatthewEvans2032@users.noreply.github.com>
2024-01-28 12:35:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+window.onscroll = function() {
+    var backToTopButton = document.getElementById("backToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
 
